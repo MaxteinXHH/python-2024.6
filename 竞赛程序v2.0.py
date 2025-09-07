a09c3afeb6f56d2841542962e593c726149b6fd4
MaxteinXHH <1265884578@qq.com>
2025-09-07 16:02:01 +0800

    Update 竞赛程序v2.0.py

--- 竞赛程序v2.0.py
+++ 竞赛程序v2.0.py
@@ -1,9 +1,15 @@
-#席好东
+#席hd
 
 #联系我 https://maxteinxhh.github.io/fun/xhd.html
 
 #我的网站 https://maxteinxhh.github.io/Xis-haohao/web/xishao.html
 
+#  2024.6 python竞赛作品。
+
+#这是一个使用 Python 编写的竞赛程序 v2.0.py ，主要用于对福布斯全球富豪榜实时数据进行分析和展示
+
+#演示视频请移至
+# https://www.bilibili.com/video/BV1pXYGz3EED/?share_source=copy_web
 
 
 
@@ -433,6 +439,7 @@ statusbar = tk.Label(status_frame, text="就绪", anchor=tk.W)
 statusbar.pack(side=tk.LEFT, fill=tk.X, expand=True, padx=5)
 
 root.mainloop()
+
 
 
 
